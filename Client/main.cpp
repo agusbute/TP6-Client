aedac4f6b63fac1019193e817fca2a97b9096ee7
Pablo Scheinfeld <scheinpablo@yahoo.com>
2018-09-11 12:18:16 -0700

    user input

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,19 +1,27 @@
 #include "stdafx.h"
 #include "Client.h"
+#include  <string>
 
 #define PORT "80"
+#define MAX_STRING_SIZE	30
 #define SERVER_IP "127.0.0.1"
 
 using namespace std;
-
+bool ip_path_validation(string * str, char ** ip, char ** path);
 int main(int argc, char* argv[])
 {
-	char * ip;				//PONER ACA IP
-	char * path;			//PONER ACA PATH
+	char * ip = (char *)calloc(MAX_STRING_SIZE, 1);
+	char * path = (char *)calloc(MAX_STRING_SIZE, 1);	
 	char * answer;
 	FILE * pFile;
 	client cl;
+	string received;
+	bool a;
 	cout << "Trying to connect to " << SERVER_IP << " on port " << PORT << std::endl;
+
+	cout << "Ingrese IP Y PATH asi: ip/path\nLuego presione ENTER" << endl;
+	cin >> received;
+	a = ip_path_validation(&received, &ip, &path);
 
 	if (cl.startConnection(SERVER_IP))
 	{
@@ -38,5 +46,27 @@ int main(int argc, char* argv[])
 	}	
 	cout << "Press Enter to close client." << std::endl;
 	getchar();
+	free(path);
+	free(ip);
 	return 0;
+}
+
+bool ip_path_validation(string * str, char ** ip, char ** path)
+{
+	int counter = 0;
+	size_t stringSize = (*str).size();
+	for (int i = 0; i <stringSize; i++)
+	{
+		if ((*str)[i] == '/')
+			counter++;
+	}
+	if (counter != 1)
+		return false;
+	size_t limitPos = str->find("/");
+	std::string ipS = str->substr(0, limitPos);
+	std::string pathS = str->substr(limitPos + 1, stringSize);
+	(*ip) = (char *)ipS.c_str();
+	(*path) = (char *)pathS.c_str();
+	return true;
+
 }
