818a200044c230e4a6da85d0e60f557db346cdbe
Defalt <Defalt@DEFALT-PC>
2018-09-11 20:33:23 -0300

    fixed client

--- Client/main.cpp
+++ Client/main.cpp
@@ -4,26 +4,27 @@
 
 #define PORT "80"
 #define MAX_STRING_SIZE	30
-#define SERVER_IP "127.0.0.1"
 
 using namespace std;
-bool ip_path_validation(string * str, char ** ip, char ** path);
+bool ip_path_validation(string * str, char * ip, char * path);
 int main(int argc, char* argv[])
 {
-	char * ip = (char *)calloc(MAX_STRING_SIZE, 1);
-	char * path = (char *)calloc(MAX_STRING_SIZE, 1);	
+	char ip[24];
+	char path[20];	
 	char * answer;
 	FILE * pFile;
 	client cl;
 	string received;
 	bool a;
-	cout << "Trying to connect to " << SERVER_IP << " on port " << PORT << std::endl;
+	
 
 	cout << "Ingrese IP Y PATH asi: ip/path\nLuego presione ENTER" << endl;
 	cin >> received;
-	a = ip_path_validation(&received, &ip, &path);
+	a = ip_path_validation(&received, ip,path);
 
-	if (cl.startConnection(SERVER_IP))
+	cout << "Trying to connect to " << ip << " on port " << PORT << std::endl;
+
+	if (cl.startConnection(ip))
 	{
 		if (cl.send_message(ip,path))
 		{
@@ -51,7 +52,7 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-bool ip_path_validation(string * str, char ** ip, char ** path)
+bool ip_path_validation(string * str, char * ip, char * path)
 {
 	unsigned int counter = 0;
 	size_t stringSize = (*str).size();
@@ -65,8 +66,8 @@ bool ip_path_validation(string * str, char ** ip, char ** path)
 	size_t limitPos = str->find("/");
 	std::string ipS = str->substr(0, limitPos);
 	std::string pathS = str->substr(limitPos + 1, stringSize);
-	(*ip) = (char *)ipS.c_str();
-	(*path) = (char *)pathS.c_str();
+	strcpy(ip, ipS.c_str());
+	strcpy(path, pathS.c_str());
 	return true;
 
 }
